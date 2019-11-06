86e1fd6d7ec9ad8672bbb504d97b1e95de5c1b2e
aalikhan <alikhanov_anar@outlook.com>
2019-11-05 22:14:36 -0800

    v1.0

--- RoboApp/View Controllers/ViewController.swift
+++ RoboApp/View Controllers/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var signUpButton: UIButton!
     
-    
     @IBOutlet weak var loginButton: UIButton!
     
     override func viewDidLoad() {
