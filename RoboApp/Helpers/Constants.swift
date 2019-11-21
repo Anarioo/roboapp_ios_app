d40f64558f6cab67b49acc05808b932ec0bb01de
Anar Alikhanov <aalikhanov@fb.com>
2019-11-21 10:38:38 -0800

    update

---
+++ RoboApp/Helpers/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  RoboApp
+//
+//  Created by Anar Alikhanov on 11/20/19.
+//  Copyright © 2019 Anar Alikhanov. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Storyboard {
+        
+        static let homeViewController = "HomeVC"
+        
+    }
+}
